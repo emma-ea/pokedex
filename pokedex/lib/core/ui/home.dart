a6087d59f1f300ba489f5db607802903684aa4ab
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-10-09 03:27:52 +0000

    setup home ui w/ bottom nav

--- pokedex/lib/core/ui/home.dart
+++ pokedex/lib/core/ui/home.dart
@@ -2,22 +2,60 @@ import 'package:flutter/material.dart';
 
 import './pokemons.dart';
 import './fav_pokemon.dart';
+import '../scoped_model/model.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({required this.model, super.key});
+
+  final PokeDataModel model;
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<Widget> screens = const [
-    Pokemons(),
-    FavPokemon()
-  ];
+  final List<Widget> screens = const [Pokemons(), FavPokemon()];
+
+  @override
+  void initState() {
+    super.initState();
+    widget.model.fetchPokemons();
+  }
+
+  int _selectedIndex = 0;
+
+  void _selectNavTab(int idx) => setState(() {
+    _selectedIndex = idx;
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Pokedex running...")));
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _bottomNavigatorItems
+            .map((item) => BottomNavigationBarItem(
+                icon: Icon(item.icon),
+                activeIcon: Icon(item.activeIcon),
+                label: item.label))
+            .toList(),
+        currentIndex: _selectedIndex,
+        onTap: _selectNavTab,
+      ),
+    );
   }
+}
+
+const _bottomNavigatorItems = <_Item>[
+  _Item('Pokemons', icon: Icons.toys_outlined, activeIcon: Icons.toys),
+  _Item('Favourite', icon: Icons.toys_outlined, activeIcon: Icons.toys),
+];
+
+class _Item {
+  final String label;
+  final IconData icon, activeIcon;
+  const _Item(this.label, {required this.icon, required this.activeIcon});
 }
