ccb54924dc86d8b95b3d9c59850e0788eaa1720f
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-10-10 17:21:14 +0000

    build custom tabs

---
+++ pokedex/lib/core/widgets/app_divider.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+
+import '../../utils/colors.dart';
+
+class AppDivider extends StatelessWidget {
+  final double? height;
+  final double thickness;
+  final double? indent;
+  final double? endIndent;
+  final bool dark;
+  final Color? color;
+
+  const AppDivider(
+      {Key? key,
+      this.height,
+      this.thickness = 1,
+      this.indent,
+      this.endIndent,
+      this.dark = false,
+      this.color})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Divider(
+      color:
+          color ?? (dark ? AppColors.darkGunmetal : AppColors.antiFlashWhite2),
+      height: height,
+      thickness: thickness,
+      indent: indent,
+      endIndent: endIndent,
+    );
+  }
+}
