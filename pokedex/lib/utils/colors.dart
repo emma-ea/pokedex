2cac91ab6b4e278dbd2eb4244b31a660f21328ca
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-10-10 17:22:02 +0000

    add colors

---
+++ pokedex/lib/utils/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const ceruleanBlue = Color(0xFF3558CD);
+  static const darkGunmetal = Color(0xFF161A33);
+  static const antiFlashWhite2 = Color(0xFFF1F1F1);
+  static const dimGray = Color(0xFF6B6B6B);
+  static const magentaDye = Color(0xFFCD2873);
+  static const platinum = Color(0xFFE8E8E8);
+  static const deepLemon = Color(0xFFEEC218);
+  static const palatinateBlue = Color(0xFF1844EE);
+  static const maximumGreen = Color(0xFF688335);
+  static const paleLavendar = Color(0xFFD5DEFF);
+  static const lavendarBlush = Color(0xFFFDF1F1);
+  static const honeyDew = Color(0xFFF3F9EF);
+  static const aliceBlue = Color(0xFFF3F9FE);
+  static const dirtyWhite = Color(0xFFDCEECF);
+  static const paleLavendar2 = Color(0xFFF79696);
+  static const greenBlue = Color(0xFF1971B9);
+}
